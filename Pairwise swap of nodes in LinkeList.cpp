ff57294d9ec7ec7204c1284dcc6c51e20a596642
Shubham Kumar <49623544+ValarMorghulis07@users.noreply.github.com>
2019-09-18 06:21:43 +0530

    Add files via upload

---
+++ Pairwise swap of nodes in LinkeList.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define mem(dp,a) memset(dp,a,sizeof(dp))
+#define pb(x) push_back(x)
+#define m_p(x,y) make_pair(x,y)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
+#define f(n) for(ll i=0;i<n;i++)
+#define r(n) for(ll j=0;j<n;j++)
+#define F first
+#define S second
+#define pi 3.14159265359
+#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+typedef long long int ll;
+ll HRX=1e18;
+ll INF=1e9+7;
+
+struct Node* pairwise_swap(struct Node* head)
+{
+   struct Node* temp=head;
+    struct Node* temp1=head;
+   while(temp!=NULL && temp->next!=NULL)
+   {
+     swap(temp->data,temp->next->data);
+     temp=temp->next->next;
+   }
+   return temp1;
+}
