d9b6cf635874a8d8b8597d9b46f4eeb30af640a1
Shubham Kumar <49623544+ValarMorghulis07@users.noreply.github.com>
2019-12-02 20:16:25 +0530

    Add files via upload

---
+++ Remove duplicates from an unsorted linked list.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define mem(dp,a) memset(dp,a,sizeof(dp))
+#define pb(x) push_back(x)
+#define m_p(x,y) make_pair(x,y)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
+#define f(n) for(ll i=0;i<n;i++)
+#define r(n) for(ll j=0;j<n;j++)
+#define F first
+#define S second
+#define pi 3.14159265359
+#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+typedef long long int ll;
+ll HRX=1e18;
+ll INF=1e9+7;
+
+
+Node *removeDuplicates(Node *root)
+{
+ Node *temp1=root,*temp2=NULL;
+ unordered_set<int>s;
+ while(temp1)
+ {
+  if(s.find(temp1->data)==s.end())
+  {
+    s.insert(temp1->data);
+    temp2=temp1;
+  }
+  else
+  {
+    temp2->next=temp1->next;
+    delete temp1;
+  }
+  temp1=temp2->next;
+ }
+ return root;
+
+}
