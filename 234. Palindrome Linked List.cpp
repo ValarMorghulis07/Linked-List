4f5445ee2559c57e41d1d729c67766e19c688ffa
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-06-22 19:49:52 +0530

    Update 234. Palindrome Linked List.cpp

--- 234. Palindrome Linked List.cpp
+++ 234. Palindrome Linked List.cpp
@@ -22,5 +22,48 @@
           return false;
       else
           return true;
+  
+    // O(n) && O(1) space
+   /*
+     if(!head || !(head->next))
+           return true;
+      bool even=1;
+      ListNode* slow=head,*fast=head;
+      while(fast && fast->next)
+      {
+        slow=slow->next;
+        fast=fast->next->next;
+      }
+      if(fast)
+      {
+       even=0;
+       slow=slow->next;
+      }
+       
+      ListNode*temp1=head,*temp2=NULL,*temp3;
+      while(temp1!=slow)
+      {
+       temp3=temp1->next;
+       temp1->next=temp2;
+       temp2=temp1;
+       temp1=temp3;
+      }
+      ListNode* cur=temp2;
+      if(!even)
+          cur=cur->next;
+      bool ff=0;
+      while(cur && slow)
+      {
+        if(cur->val!=slow->val)
+            ff=1;
+        cur=cur->next;
+        slow=slow->next;
+      }
+      if(ff)
+          return false;
+      else
+          return true;
+          */
+          
 
     }
