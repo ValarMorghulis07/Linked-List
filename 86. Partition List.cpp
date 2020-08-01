336a9421de71b3f596899e17f5f82330d10e6f5c
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-02 00:31:36 +0530

    Update 86. Partition List.cpp

--- 86. Partition List.cpp
+++ 86. Partition List.cpp
@@ -1,56 +1,61 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* partition(ListNode* head, int x)
+    ListNode* partition(ListNode* head, int x) 
     {
-      ListNode* temp=head;
-      if(head==NULL)
-          return NULL;
-       ListNode* bs=NULL;
-       ListNode* be=NULL;
-       ListNode* as=NULL;
-       ListNode* ae=NULL;
-      while(temp)
+     if(!head || !head->next)
+         return head;
+     ListNode* temp=head,*as=NULL,*ae=NULL,*bs=NULL,*be=NULL;
+     while(temp)
+     {
+      if(temp->val<x)
       {
-       if(temp->val<x)
+       ListNode* temp1=new ListNode(temp->val);
+       if(!as)
        {
-         ListNode* temp1=new ListNode(x);;
-         if(bs==NULL)
-         {
-          bs=temp1;
-          be=temp1;
-         }
-         else
-         {
-           be->next=temp1;
-           be=temp1;
-         }
-
+        as=temp1;
+        ae=temp1;
        }
-          else
-          {
-           ListNode* temp1=new ListNode(x);;
-           if(as==NULL)
-           {
-             as=temp1;
-             ae=temp1;
-           }
-          else
-          {
-           ae->next=temp1;
-           ae=temp1;
-          }
-
-          }
-          temp=temp->next;
+       else
+       {
+        ae->next=temp1;
+        ae=temp1;
+       }
       }
-        if(be==NULL)
-            return as;
-        else
-        {
-         be->next=as;
-            return bs;
-        }
-
+      else
+      {
+       ListNode* temp1=new ListNode(temp->val);
+       if(!bs)
+       {
+         bs=temp1;
+         be=temp1;
+       }
+       else
+       {
+         be->next=temp1;
+         be=temp1;
+       }
+      }
+      temp=temp->next;
+     }
+     if(!as)
+         return bs;
+     else
+     {
+      ae->next=bs;
+      return as;
+      
+     }
+     
+         
     }
 };
