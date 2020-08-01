883628bc2fe6dc1e99301f63dbc87c0721ace41f
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-01 20:06:22 +0530

    Update Delete N nodes after M nodes of a linked list.cpp

--- Delete N nodes after M nodes of a linked list.cpp
+++ Delete N nodes after M nodes of a linked list.cpp
@@ -1,40 +1,32 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
 
-//M!=0
 void linkdelete(struct Node  *head, int M, int N)
 {
-  struct Node *cur=head,*temp;
-  int i;
-  while(cur)
+  if(!head)
+   return;
+  struct Node* temp=head,*temp1;
+  int idx;
+  while(temp)
   {
-    for(i=1;i<M && cur!=NULL;i++)
-     cur=cur->next;
-    if(cur==NULL)
-     return;
-    temp=cur->next;
-    for(i=1;i<=N && temp!=NULL;i++)
-    {
-     Node *p=temp;
+   idx=1;
+   while(idx<M && temp->next)
+   {
      temp=temp->next;
-     delete p;
-    }
-    cur->next=temp;
-    cur=temp;
-
+     idx++;
+   }
+   if(temp==NULL)
+    return;
+   idx=0;
+   temp1=temp;
+   while(idx<N && temp->next)
+   {
+    
+    temp=temp->next;
+    Node* p=temp;
+    delete p;
+    idx++;
+   }
+   temp1->next=temp->next;
+   temp=temp->next;
   }
 }
+
