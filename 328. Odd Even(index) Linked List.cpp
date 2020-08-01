29bc9b982ae10b9349b3f955a1b5e64f715d1bc2
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-01 13:58:29 +0530

    Update 328. Odd Even(index) Linked List.cpp

--- 328. Odd Even(index) Linked List.cpp
+++ 328. Odd Even(index) Linked List.cpp
@@ -42,3 +42,20 @@ public:
 
     }
 };
+
+// This is the actual solution
+ ListNode* oddEvenList(ListNode* head)
+    {
+     if(!head || !head->next || !head->next->next)
+         return head;
+     ListNode* odd=head,*even=head->next,*evenHead=even;
+     while(even && even->next)
+     {
+      odd->next=even->next;
+      odd=even->next;
+      even->next=odd->next;
+      even=odd->next;
+     }
+     odd->next=evenHead;
+     return head;
+    }
