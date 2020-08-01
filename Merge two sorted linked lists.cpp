b1d10ed153d6f4c1d5f13fd7327cbc20a8d19b35
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-01 17:42:58 +0530

    Update Merge two sorted linked lists.cpp

--- Merge two sorted linked lists.cpp
+++ Merge two sorted linked lists.cpp
@@ -1,48 +1,54 @@
- ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
     {
-      ListNode* cur;
-       ListNode* ff=l1;
-       ListNode* ss=l2;
-      if(ff && ss)
+     ListNode* head=NULL,*cur;
+     if(!l1 && !l2)
+         return NULL;
+     if(!l1 && l2)
+         return l2;
+     if(l1 && !l2)
+         return l1;
+     if(l1->val<l2->val)
+     {
+      head=l1;
+      cur=l1;
+      l1=l1->next;
+     }
+     else
+     {
+      head=l2;
+      cur=l2;
+      l2=l2->next;
+     }
+     while(l1 && l2)
+     {
+      if(l1->val<l2->val)
       {
-      if(ff->val<ss->val)
-      {
-       cur=ff;
-       ff=ff->next;
+       cur->next=l1;
+       l1=l1->next;
       }
-      else 
-      {
-        cur=ss;
-        ss=ss->next;
-      }
-      }
-      else if(ff && !ss)
-          return ff;
-      else if(!ff && ss)
-          return ss;
-      else if(!ff && !ss)
-          return NULL;
-          
-      ListNode* start=cur;
-      while(ff && ss)
-      {
-        if(ff->val<ss->val)
-       {
-       cur->next=ff;
-       ff=ff->next;
-       cur=cur->next;
-       }
       else
       {
-        cur->next=ss;
-        ss=ss->next;
-        cur=cur->next;
+       cur->next=l2;
+       l2=l2->next;
       }
-      }
-      if(ff)
-         cur->next=ff;
-      if(ss)
-          cur->next=ss;
-      return start;
-      
+      cur=cur->next;
+     }
+     if(l1)
+         cur->next=l1;
+     else
+         cur->next=l2;
+     return head;
     }
+};
