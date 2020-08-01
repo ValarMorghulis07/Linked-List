63886bd9127194ce6cba957123d857581ea74e05
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-01 20:35:35 +0530

    Update Add two numbers represented by linked lists.cpp

--- Add two numbers represented by linked lists.cpp
+++ Add two numbers represented by linked lists.cpp
@@ -1,48 +1,44 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
-
-Node *create(int x)
-{
- Node *temp=new Node(x);
- temp->next=NULL;
- return temp;
-}
-Node* addTwoLists(Node* first, Node* second)
-{
- Node *temp=NULL,*head=NULL,*last=NULL;
- int sum=0,carry=0;
- while(first!=NULL || second!=NULL)
- {
-  sum=carry+(first ? first->data:0)+(second ? second->data:0);
-  carry=(sum>=10) ? 1:0;
-  sum=sum%10;
-  temp=create(sum);
-  if(head==NULL)
-   head=temp;
-  else
-   last->next=temp;
-  last=temp;
-  if(first)
-    first=first->next;
-  if(second)
-    second=second->next;
- }
- if(carry>0)
-  temp->next=create(carry);
-  return head;
-
-}
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+    {
+     int sum=0,carry=0;
+     ListNode* head=NULL,*last=NULL,*temp=NULL;
+     while(l1 || l2)
+     {
+      sum=carry+(l1 ? l1->val:0)+(l2 ? l2->val:0);
+      carry=(sum>=10) ? 1:0;
+      sum=sum%10;
+      temp=new ListNode(sum);
+      if(!head)
+      {
+       head=temp;
+       last=temp;
+      }
+      else
+      {
+       last->next=temp;
+       last=temp;
+      }
+      if(l1)
+          l1=l1->next;
+      if(l2)
+          l2=l2->next;
+      
+     }
+     if(carry==1)
+         last->next=new ListNode(1);
+     return head;
+         
+    }
+};
