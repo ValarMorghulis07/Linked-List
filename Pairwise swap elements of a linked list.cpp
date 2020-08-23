1ba67fd51e8e161bb0804e23202d4d38453f5ea9
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-23 23:28:08 +0530

    Update Pairwise swap elements of a linked list.cpp

--- Pairwise swap elements of a linked list.cpp
+++ Pairwise swap elements of a linked list.cpp
@@ -1,19 +1,5 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
+// Pairwise swap elements of a linked list
+
 
 Node* pairWiseSwap(struct Node* head)
 {
@@ -25,3 +11,47 @@ Node* pairWiseSwap(struct Node* head)
   }
   return head;
 }
+
+// Pairwise swap of nodes in LinkeList
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) 
+    {
+    // RECURSIVE 
+     /*if(!head)
+         return NULL;
+     ListNode* temp1=head,*temp2=NULL,*temp3=NULL;
+     int cnt=0;
+     while(temp1 && cnt<2)
+     {
+      temp3=temp1->next;
+      temp1->next=temp2;
+      temp2=temp1;
+      temp1=temp3;
+      cnt++;
+     }
+     if(temp3)
+         head->next=swapPairs(temp3);
+     return temp2;*/
+     // ITERATIVE
+        
+     if(!head || !head->next)
+         return head;
+     ListNode* temp1=head,*temp2=NULL,*temp3=NULL;
+     while(temp1 && temp1->next)
+     {
+      temp3=temp1->next;
+      temp1->next=temp3->next;
+      temp3->next=temp1;
+      if(temp2)
+          temp2->next=temp3;
+      else
+          head=temp3;
+      temp2=temp1;
+      temp1=temp1->next;
+     }
+     return head;
+        
+    }
+};
