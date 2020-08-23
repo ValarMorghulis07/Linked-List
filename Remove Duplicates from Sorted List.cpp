53f0b53c5f8abe1da8118ecf428be163b012f57b
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-23 23:34:33 +0530

    Update Remove Duplicates from Sorted List.cpp

--- Remove Duplicates from Sorted List.cpp
+++ Remove Duplicates from Sorted List.cpp
@@ -69,3 +69,28 @@ public:
      return start;
     }
 };
+
+// Remove duplicates from an unsorted linked list
+
+Node * removeDuplicates( Node *head) //O(n) && O(n)
+{
+ if(!head || !head->next)
+  return head;
+ unordered_set<int>ss;
+ Node *temp1=head,*temp2=NULL;
+ while(temp1)
+ {
+  if(ss.find(temp1->data)==ss.end())
+  {
+    ss.insert(temp1->data);
+    temp2=temp1;
+  }
+  else
+  {
+   temp2->next=temp1->next;
+   delete temp1;
+  }
+  temp1=temp2->next;
+ }
+ return head;
+}
