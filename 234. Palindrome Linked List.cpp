27f757e00f5ad1d721f1a353fd3d8f3541d92c47
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-09-12 18:30:32 +0530

    Update 234. Palindrome Linked List.cpp

--- 234. Palindrome Linked List.cpp
+++ 234. Palindrome Linked List.cpp
@@ -25,17 +25,8 @@
           return true;
  }
   
-    // O(n) && O(1) space
-   /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+  // O(n) && O(1) space
+   
 class Solution {
 public:
     bool isPalindrome(ListNode* head) 
