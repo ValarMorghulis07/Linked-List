8ba03c2117dfce4aaf796ee1e338086dc392c55c
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-09-18 00:38:34 +0530

    Update 138. Copy List with Random Pointer.cpp

--- 138. Copy List with Random Pointer.cpp
+++ 138. Copy List with Random Pointer.cpp
@@ -32,22 +32,6 @@ public:
 
 // o(n) && o(1)
 
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
-
 class Solution {
 public:
     Node* copyRandomList(Node* head) 
